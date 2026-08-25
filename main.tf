1ef05f3fff753366d8c245529f74f5212e55a516
sarvin901 <arvind.singhk198@gmail.com>
2026-08-25 13:18:25 +0530

    add

--- main.tf
+++ main.tf
@@ -48,6 +48,14 @@ resource "azurerm_storage_account" "stg1" {
 
 }
 
+<<<<<<< HEAD
+=======
+resource "azurerm_resource_group" "rarf" {
+  name = "aDF"
+  location = "Central India"
+  
+}
+>>>>>>> 9dfd7ee (modified)
 
 
 resource "azurerm_subnet" "subnet" {
