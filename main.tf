845062a5373ce4611eed0108c2da13e96ae4c49a
sarvin901 <arvind.singhk198@gmail.com>
2026-08-26 01:03:42 +0530

    add

--- main.tf
+++ main.tf
@@ -1,7 +1,7 @@
 terraform {
   required_providers {
     azurerm = {
-      source = "hashicorp/azurerm"
+      source  = "hashicorp/azurerm"
       version = "4.68.0"
     }
   }
@@ -9,68 +9,83 @@ terraform {
 
 provider "azurerm" {
   features {}
+
   subscription_id = "7d2b7ec8-30a6-4bc2-9251-5eb16f970054"
 }
 
+# Resource Group 1
 resource "azurerm_resource_group" "rg" {
   name     = "nrtest"
   location = "eastus"
 }
+
+# Resource Group 2
 resource "azurerm_resource_group" "rg1" {
   name     = "amitest123"
   location = "eastus"
 }
 
+# Resource Group for VNet/Subnet
+resource "azurerm_resource_group" "rg2" {
+  name     = "network-rg"
+  location = "eastus"
+}
+
+# Storage Account 1
 resource "azurerm_storage_account" "sstss" {
   name                     = "amittestsa1232"
   resource_group_name      = azurerm_resource_group.rg1.name
   location                 = azurerm_resource_group.rg1.location
   account_tier             = "Standard"
   account_replication_type = "GRS"
-
 }
 
+# Storage Account 2
 resource "azurerm_storage_account" "ssts1" {
-  depends_on               = [azurerm_resource_group.rg1]
+  depends_on = [azurerm_resource_group.rg1]
+
   name                     = "amittestsa1233242"
-  resource_group_name      = "amitest123"
-  location                 = "eastus"
+  resource_group_name      = azurerm_resource_group.rg1.name
+  location                 = azurerm_resource_group.rg1.location
   account_tier             = "Standard"
   account_replication_type = "GRS"
 }
+
+# Storage Account 3
 resource "azurerm_storage_account" "stg1" {
-  depends_on               = [azurerm_resource_group.rg1]
+  depends_on = [azurerm_resource_group.rg1]
+
   name                     = "amitstg1234445"
-  resource_group_name      = "amitest123"
-  location                 = "eastus"
+  resource_group_name      = azurerm_resource_group.rg1.name
+  location                 = azurerm_resource_group.rg1.location
   account_tier             = "Standard"
   account_replication_type = "GRS"
-
 }
 
+# Additional Resource Group
 resource "azurerm_resource_group" "rarf" {
-  name = "aDF"
+  name     = "aDF"
   location = "Central India"
-  
 }
 
+# Virtual Network
+resource "azurerm_virtual_network" "vnet" {
+  name                = "amitest-vnet"
+  location            = azurerm_resource_group.rg2.location
+  resource_group_name = azurerm_resource_group.rg2.name
+  address_space       = ["10.0.0.0/16"]
+}
 
-
-
-
+# Subnet
 resource "azurerm_subnet" "subnet" {
-  depends_on          = [azurerm_virtual_network.vnet]
   name                 = "amitestsubnet"
   resource_group_name  = azurerm_resource_group.rg2.name
   virtual_network_name = azurerm_virtual_network.vnet.name
   address_prefixes     = ["10.0.1.0/24"]
 }
 
-
-
-
+# Additional Resource Group
 resource "azurerm_resource_group" "arvi" {
-  name = "arvind1"
-  location = "south india"
-  
+  name     = "arvind1"
+  location = "southindia"
 }
