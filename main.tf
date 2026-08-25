97b75b5fecbcc8684bea74818cb6ab961435958e
sarvin901 <arvind.singhk198@gmail.com>
2026-08-26 00:53:06 +0530

    add

--- main.tf
+++ main.tf
@@ -64,6 +64,9 @@ resource "azurerm_subnet" "subnet" {
   address_prefixes     = ["10.0.1.0/24"]
 }
 
+
+
+
 resource "azurerm_resource_group" "arvi" {
   name = "arvind1"
   location = "south india"
